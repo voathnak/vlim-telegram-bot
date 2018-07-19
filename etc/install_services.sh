5b5a1962f1007990b863edb1273a411debb25094
vlim <voathnak.lim@gmail.com>
2018-07-19 13:28:01 +0700

    [TASK] change env to python 3

--- etc/install_services.sh
+++ etc/install_services.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 apt-get install virtualenv && \
-virtualenv /projects/vlim-telegram-bot.env && \
+virtualenv /projects/vlim-telegram-bot.env -p python3 && \
 source /projects/vlim-telegram-bot.env/bin/activate && \
 /projects/vlim-telegram-bot.env/bin/pip install -r /projects/vlim-telegram-bot/requirement.txt && \
 cd /projects/vlim-telegram-bot
